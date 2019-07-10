367261cf2dc1e8da4df80fa449aa062fc186ad57
AHBruns <a.bruns@topl.me>
2019-07-10 17:50:17 -0500

    AB :: name change

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.tsort/
-cd TSort/src
-chmod +x tsort
-cp -f tsort ~/.tsort/
-cp -f node.py ~/.tsort/
-cp -f read_write.py ~/.tsort/
+mkdir -p ~/.melzi/
+cd Melzi/src
+chmod +x melze
+cp -f tsort ~/.melzi/
+cp -f node.py ~/.melzi/
+cp -f read_write.py ~/.melzi/
 ln -s -f ~/.tsort/tsort /usr/local/bin/tsort
-ln -s -f ~/.tsort/tsort /usr/local/bin/moen
-ln -s -f ~/.tsort/tsort /usr/local/bin/moenize
+ln -s -f ~/.tsort/tsort /usr/local/bin/melzi
 cd ../..
-rm -rf TSort/
+rm -rf Melzi/
